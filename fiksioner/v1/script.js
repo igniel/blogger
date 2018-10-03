1cb928a53c4d8a99d8a8ee09a2da1e5d78407ad4
Rain <himemiyarain@gmail.com>
2018-10-03 18:14:55 +0700

    Create script.js

---
+++ fiksioner/v1/script.js
@@ -0,0 +1,28 @@
+// jQuery
+var fiksioner = document.createElement('script');
+fiksioner.src = 'https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js';
+fiksioner.async = true;
+document.getElementsByTagName('body')[0].prepend(fiksioner);
+
+!function(){
+  // Menu
+  document.getElementById('fiksionerMenu').addEventListener('click', function(){
+    if (this.className != 'open'){
+      document.querySelectorAll('.tabs li').forEach(function(a){a.classList.add('show');});
+      this.classList.add('open');
+    }
+    else {
+      document.querySelectorAll('.tabs li').forEach(function(a){a.classList.remove('show');});
+      this.classList.remove('open');
+    }
+  });
+
+  // Back To Top
+  $(document).scroll(function() {
+  return $(document).scrollTop() > 300 ? $('.ignielToTop').addClass('show') : $('.ignielToTop').removeClass('show')
+  }), $('.ignielToTop').click(function() {
+    return $('html,body').animate({
+      scrollTop: '0'
+    });
+});
+}(window.jQuery);
